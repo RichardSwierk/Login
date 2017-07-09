dc8d69378fd2d0f9d47ff9ef6d95e9cbce9b43a8
18kills <18kills.18kills@gmail.com>
2017-07-08 20:39:37 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,11 +21,10 @@ echo '           \____/'
 echo ''
 sleep 2
 
-if ls | grep login.sh
-
 sudo apt-get update
-sudo apt-get install hydra pnscan grep --yes
+sudo apt-get install hydra pnscan grep wget bzip2 --yes
+wget downloads.skullsecurity.org/passwords/rockyou.txt.bz2
+bzip2 -d rockyou.txt.bz2
 chmod +x login.sh
-
 
 
