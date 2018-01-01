c0c38ddfb27b54d8e2031291ff5e5cf42fc5da44
Richard T Swierk <18kills.18kills@gmail.com>
2018-01-01 17:30:33 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,4 @@
 #!/bin/bash
-#Group: Hive
-
-echo ''
-echo '   ░█─░█ ▀█▀ ░█──░█ ░█▀▀▀'
-echo '   ░█▀▀█ ░█─ ─░█░█─ ░█▀▀▀' 
-echo '   ░█─░█ ▄█▄ ──▀▄▀─ ░█▄▄▄' 
-echo '            ____'
-echo '           /    \'
-echo '      ____/      \____'
-echo '     /    \      /    \'
-echo '    /      \____/      \'
-echo '    \      /    \      /'
-echo '     \____/      \____/'
-echo '     /    \      /    \'
-echo '    /      \____/      \'
-echo '    \      /    \      /'
-echo '     \____/      \____/'
-echo '          \      /'
-echo '           \____/'
-echo ''
-sleep 2
-
 sudo apt-get update
 sudo apt-get install hydra pnscan grep wget bzip2 --yes
 wget downloads.skullsecurity.org/passwords/rockyou.txt.bz2
